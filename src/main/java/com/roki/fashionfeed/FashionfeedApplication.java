4550d795ac59548172810501288d12bf6550b909
김경록 <goodboy3421@gmail.com>
2020-03-18 20:28:02 +0900

    Initial commit
    fashion feed 웹프로젝트 환경설정 완료

---
+++ src/main/java/com/roki/fashionfeed/FashionfeedApplication.java
@@ -0,0 +1,13 @@
+package com.roki.fashionfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionfeedApplication.class, args);
+    }
+
+}
