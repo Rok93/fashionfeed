66ee025ea5ecea44eeac197bbecab96c9caabdff
김경록 <goodboy3421@gmail.com>
2020-03-21 20:31:04 +0900

    feat: feed 엔티티에 feedTitle필드, orphanRemoval 속성추가
    feedTitle필드 추가 , orphanRemoval 속성변경

--- src/main/java/com/roki/fashionfeed/domain/feed/Feed.java
+++ src/main/java/com/roki/fashionfeed/domain/feed/Feed.java
@@ -2,7 +2,6 @@ package com.roki.fashionfeed.domain.feed;
 
 import com.roki.fashionfeed.domain.BaseTimeEntity;
 import com.roki.fashionfeed.domain.chat.Chat;
-import com.roki.fashionfeed.domain.likefull.LikeType;
 import com.roki.fashionfeed.domain.likefull.Likefull;
 import com.roki.fashionfeed.domain.share.Share;
 import lombok.Builder;
@@ -21,48 +20,30 @@ public class Feed extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //feedTag (List로 넣을 수 있나..?)
+    @Column(length = 100, nullable = false)
+    private String feedTitle;
 
     @Column(length = 255, nullable = false)
-    private String feedText;
+    private String feedContent;
 
     @Column(nullable = false)
     private String feedImage;
 
-    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Chat> chats = new ArrayList<>();
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "likefull_id")
-    private Likefull likefull; //좋아요 개수를 파악하는 방법 Linkfull 모두 조회해서 카운트하기?
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Likefull> likefulls = new ArrayList<>(); //좋아요 개수를 파악하는 방법 Linkfull 모두 조회해서 카운트하기?
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "share_id")
-    private Share share;
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Share> shares = new ArrayList<>();
 
     @Builder
-    public Feed(String feedText, String feedImage, Chat feedChat, Likefull feedLikefull
-            , Share feedShare) {
-        this.feedText = feedText;
+    public Feed(String feedTitle, String feedContent, String feedImage) {
+        this.feedTitle = feedTitle;
+        this.feedContent = feedContent;
         this.feedImage = feedImage;
-        addChats(feedChat);
-        this.likefull = feedLikefull;
-        this.share = feedShare;
     }
 
-    public void addChats(Chat feedChat) {
-        chats.add(feedChat);
-        feedChat.setFeed(this);
-    }
-
-    //    private void setFeedLikefull(Likefull feedLikefull) {
-//        this.likefull = feedLikefull;
-//        feedLikefull.setFeed(this);
-//    }
-//
-//    private void setFeedShare(Share share) {
-//        this.share = share;
-//        share.setFeed(this);
-//    }
 }
 
