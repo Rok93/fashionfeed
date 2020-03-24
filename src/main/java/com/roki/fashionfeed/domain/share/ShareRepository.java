7344ed38a07a8a775d6aa66d6aece4adf72aec94
김경록 <goodboy3421@gmail.com>
2020-03-24 22:59:15 +0900

    feat: 공유정보 삭제기능 추가
    피드 아이디 정보와 유저 아이디정보를 이용한 공유정보 삭제기능

--- src/main/java/com/roki/fashionfeed/domain/share/ShareRepository.java
+++ src/main/java/com/roki/fashionfeed/domain/share/ShareRepository.java
@@ -3,4 +3,6 @@ package com.roki.fashionfeed.domain.share;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShareRepository extends JpaRepository<Share, Long> {
+
+    void deleteByUserIdAndFeed_Id(Long userId, Long feedId);
 }
