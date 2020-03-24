9f8c672ae788316e8584c2bb69b23d1f0a224769
김경록 <goodboy3421@gmail.com>
2020-03-24 23:23:23 +0900

    feat: FeedApiController 추가
    Feed의 Paging에 따라 데이터를 조회할 수 있다, Test 데이터를 받을 수 있다.

---
+++ src/main/java/com/roki/fashionfeed/web/FeedApiController.java
@@ -0,0 +1,31 @@
+package com.roki.fashionfeed.web;
+
+import com.roki.fashionfeed.service.FeedService;
+import com.roki.fashionfeed.web.dto.FeedResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+public class FeedApiController {
+    private final FeedService feedService;
+
+    @GetMapping("/test") //Feed 20개 저장
+    public void testSave(HttpServletResponse response) throws IOException {
+        feedService.saveTestFeeds();
+        response.sendRedirect("/");
+    }
+
+    @GetMapping("/api/feed")
+    public Page<FeedResponseDto> testSave(@PageableDefault(page = 0, size = 10, sort = "id",
+            direction = Sort.Direction.DESC) Pageable pageable) { //이부분 출력 테스트를 해봐야할 것같다!
+        return feedService.findAll(pageable);
+    }
+}
