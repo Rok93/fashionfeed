75dcdf6cc7954cb05d2918774f85297c922f8c3a
김경록 <goodboy3421@gmail.com>
2020-03-24 23:21:21 +0900

    feat: IndexController 추가
    index 화면의 컨트롤러

---
+++ src/main/java/com/roki/fashionfeed/web/IndexController.java
@@ -0,0 +1,26 @@
+package com.roki.fashionfeed.web;
+
+import com.roki.fashionfeed.service.FeedService;
+import com.roki.fashionfeed.web.dto.FeedResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    private final FeedService feedService;
+
+    @GetMapping("/")
+    public String findFeedByPageable(@PageableDefault(page = 0, size = 10, sort = "id",
+            direction = Sort.Direction.DESC) Pageable pageable, Model model) {
+        Page<FeedResponseDto> feedPage = feedService.findAll(pageable);
+        model.addAttribute("feedPage", feedPage);
+        return "index";
+    }
+}
