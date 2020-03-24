540896c49eb96add2d953823b8cb198c27258161
김경록 <goodboy3421@gmail.com>
2020-03-24 22:58:42 +0900

    feat: 좋아요 삭제기능 추가
    피드 아이디 정보와 유저 아이디정보를 이용한 좋아요 삭제기능

--- src/main/java/com/roki/fashionfeed/domain/likefull/LikefullRepository.java
+++ src/main/java/com/roki/fashionfeed/domain/likefull/LikefullRepository.java
@@ -3,4 +3,8 @@ package com.roki.fashionfeed.domain.likefull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikefullRepository extends JpaRepository<Likefull, Long> {
+
+    Likefull findByUserIdAndFeed_Id(Long userId, Long feedId);
+
+    void deleteByUserIdAndFeed_Id(Long userId, Long feedId);
 }
