3dfd7ab079cc01cf66d031de6e0b24331caad9d1
김경록 <goodboy3421@gmail.com>
2020-08-30 06:51:44 +0900

    fix: oauth와 충돌되는 맵핑 주소 변경
    /login 의 경우 OAuth의 url로 설정해놔서 충돌이 남 /login-form으로 수정

--- src/main/java/com/roki/fashionfeed/web/LoginController.java
+++ src/main/java/com/roki/fashionfeed/web/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping(value = "/login")
+    @GetMapping(value = "/login-form")
     public String login() {
         return "login-form";
     }
